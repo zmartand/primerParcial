f0c7e9df2a19b8befabf63fbb80fcd93f5db5eb2
Zara Martín <zmartand@myuax.es>
2023-02-23 19:22:55 +0100

    Ejercicio1

---
+++ src/main/java/aplicacion/Principal.java
@@ -0,0 +1,10 @@
+package aplicacion;
+import mates.Matematicas;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        Matematicas m = new Matematicas();
+        m.MetodoMontecarlo();
+    }
+}
